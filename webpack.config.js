43ae2c132fd916c2d4fbab25cdb4cea725c0d4a7
Estevan Maito <ejmaito@gmail.com>
2020-05-19 22:46:19 -0300

    fix(button): add named export

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    library: 'tailwindCssReactUi',
+    library: 'tailwindcss-react-ui',
     libraryTarget: 'umd',
   },
   externals: {
