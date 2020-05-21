2c85a9d2492ac39a13ff06d3b637af736f6c28b4
Estevan Maito <ejmaito@gmail.com>
2020-05-20 21:45:49 -0300

    fix(style): add styleguide docs

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,18 @@
 const path = require('path')
 
 module.exports = {
+  styleguideDir: 'docs',
+  title: 'Tailwind CSS React UI',
+  styles: {
+    Code: {
+      code: {
+        color: 'rgb(128, 90, 213)',
+        backgroundColor: 'rgb(247, 250, 252)',
+        padding: '0 4px',
+        fontFamily: 'Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace',
+      },
+    },
+  },
   require: [path.join(__dirname, 'style/output.css')],
   webpackConfig: {
     module: {
